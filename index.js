450df08faba1e99514ebcf9b993d7532eb7d2953
unknown <derek.slauson5@gmail.com>
2022-08-06 09:35:31 -0500

    Moved use(postsRoute) below use(cors()) in index.js to fix cors issue

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ dotenv.config()
 import postsRoutes from "./routes/posts.js"
 
 const app = express()
-app.use("/posts", postsRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use("/posts", postsRoutes)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
